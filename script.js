c3b2aa96b3bccefcabad308d41b3e7a416255f5a
umeshsir05 <ukbanjare1989@gmail.com>
2026-03-15 20:51:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,6 +50,7 @@
         { month: 3, day: 15, event: "World Consumer Rights Day" },
         { month: 3, day: 15, event: "International Day Against Police Brutality" },
         { month: 3, day: 15, event: "Bhakt Mata Karma Jayanti (2024)" },
+{ month: 3, day: 16, event: "First day of visiting School gld (India)" },
         { month: 3, day: 18, event: "Ordnance Factories Day (India)" },
         { month: 3, day: 20, event: "International Day of Happiness" },
         { month: 3, day: 21, event: "World Forestry Day" },
